23b57486be80eba85ef14fc65e8c89f6047d61a6
Eodera1 <ericowino8@gmail.com>
2023-06-22 08:16:32 -0700

    Stack updated files

---
+++ monty_errors1.c
@@ -0,0 +1,136 @@
+#include "monty.h"
+
+/**
+ * _push - pushes an element to the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _push(stack_t **doubly, unsigned int cline)
+{
+	int m, i;
+
+	if (!vglo.arg)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "usage: push integer\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; vglo.arg[i] != '\0'; i++)
+	{
+		if (!isdigit(vglo.arg[i]) && vglo.arg[i] != '-')
+		{
+			dprintf(2, "L%u: ", cline);
+			dprintf(2, "usage: push integer\n");
+			free_vglo();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	m = atoi(vglo.arg);
+
+	if (vglo.lifo == 1)
+		add_dnodeint(doubly, m);
+	else
+		add_dnodeint_end(doubly, m);
+}
+
+/**
+ * _pall - prints all values on the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line numbers
+ * Return: no return
+ */
+void _pall(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+	(void)cline;
+
+	aux = *doubly;
+
+	while (aux)
+	{
+		printf("%d\n", aux->m);
+		aux = aux->next;
+	}
+}
+
+/**
+ * _pint - prints the value at the top of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pint(stack_t **doubly, unsigned int cline)
+{
+	(void)cline;
+
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*doubly)->m);
+}
+
+/**
+ * _pop - removes the top element of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pop(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+
+	if (doubly == NULL || *doubly == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	free(aux);
+}
+
+/**
+ * _swap - swaps the top two elements of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _swap(stack_t **doubly, unsigned int cline)
+{
+	int k = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, k++)
+		;
+
+	if (k < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	aux->next = (*doubly)->next;
+	aux->prev = *doubly;
+	(*doubly)->next = aux;
+	(*doubly)->prev = NULL;
+}
---
+++ monty_functions2.c
@@ -0,0 +1,73 @@
+#include "monty.h"
+/**
+ * _calloc - concatenate tw strings specially
+ * @nmemb: number of elements
+ * @size: type of elements
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *s = NULL;
+	unsigned int j;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < (nmemb * size); j++)
+	{
+		*((char *)(s) + j) = 0;
+	}
+	return (s);
+}
+/**
+ * _realloc - change the size and copy the content
+ * @ptr: malloc pointer to reallocate
+ * @old_size: old number of bytes
+ * @new_size: new number of Bytes
+ * Return: nothing
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s = NULL;
+	unsigned int j;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		if (!s)
+			return (NULL);
+		return (s);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		s = malloc(new_size);
+		if (!s)
+			return (NULL);
+		for (j = 0; j < old_size; j++)
+			p[j] = *((char *)ptr + j);
+		free(ptr);
+	}
+	else
+	{
+		s = malloc(new_size);
+		if (!s)
+			return (NULL);
+		for (j = 0; j < new_size; j++)
+			p[j] = *((char *)ptr + j);
+		free(ptr);
+	}
+	return (s);
+}
